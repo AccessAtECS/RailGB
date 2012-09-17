5a8e58e0950a9f0f58a2f2bc2c352d0eb577ac48
Russell Newman <rn2@ecs.soton.ac.uk>
2012-09-17 11:31:44 +0100

    Initial Bootstrap version
    Signed-off-by: Russell Newman <rn2@ecs.soton.ac.uk>

--- index.php
+++ index.php
@@ -1,32 +1,40 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml">
-  <head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-    <title>Google Maps V3 API Sample</title>
-    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
-    <script type="text/javascript">
-
-      function initialize() {
-        var mapDiv = document.getElementById('map-canvas');
-        var map = new google.maps.Map(mapDiv, {
-          center: new google.maps.LatLng(37.4419, -122.1419),
-          zoom: 13,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        });
-      
-        var marker = new google.maps.Marker({
-          map: map,
-          position: new google.maps.LatLng(37.4419, -122.1419),
-          draggable: true
-        });
-      }
-      
-
-      google.maps.event.addDomListener(window, 'load', initialize);
-    </script>
-  </head>
-  <body style="font-family: Arial; border: 0 none;">
-    <div id="map-canvas" style="width: 500px; height: 400px"></div>
-  </body>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<link href="/railgb/css/bootstrap.css" rel="stylesheet">
+		<link href="/railgb/css/bootstrap-responsive.css" rel="stylesheet">
+		<script type="text/javascript" src="/railgb/js/jquery-1.8.1.min.js"></script>
+		<script type="text/javascript" src="/railgb/js/bootstrap.min.js"></script>
+		<title>Google Maps V3 API Sample</title>
+		<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+		<script type="text/javascript">
+			function initialize() {
+				var mapDiv = document.getElementById('map-canvas');
+				var map = new google.maps.Map(mapDiv, {
+					center: new google.maps.LatLng(52.84923, -2.032471),
+					zoom: 7,
+					mapTypeId: google.maps.MapTypeId.ROADMAP
+				});
+			}
+			
+			google.maps.event.addDomListener(window, 'load', initialize);
+		</script>
+	</head>
+	<body>
+		<div class="container">
+			<div class="page-header">
+					<h1>RailGB <small>Accessible Rail</small></h1>
+				</div>
+			<div class="row-fluid">
+				<div class="span3">
+					Sidebar!
+				</div>
+				<div class="span8">
+					<div id="map-canvas" style="width: 700px; height: 900px"></div>
+				</div>
+				</div>
+			
+		</div>
+	</body>
 </html>
 ​
