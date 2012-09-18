f63e43ed7607482ea7529f6235121cc828e02aaa
Magnus P White <mpw@ecs.soton.ac.uk>
2012-09-18 15:04:44 +0100

    About pagae
    Signed-off-by: Magnus P White <mpw@ecs.soton.ac.uk>

--- about/index.php
+++ about/index.php
@@ -1,33 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+		<link rel="icon" href="/railgb/img/theme/favicon.png" type="image/x-icon">
 		<link href="/railgb/css/bootstrap.css" rel="stylesheet">
 		<link href="/railgb/css/bootstrap-responsive.css" rel="stylesheet">
 		<link href="/railgb/css/railgb.css" rel="stylesheet">
-		<script type="text/javascript" src="/railgb/js/jquery-1.8.1.min.js"></script>
-		<script type="text/javascript" src="/railgb/js/bootstrap.min.js"></script>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		
 		<title>RailGB - Accessible Rail Network Map</title>
 	</head>
+
 	<body>
-		<div class="container " id="container">
+		<div class="container" id="container">
 			<div class="page-header">
-					<h1>RailGB <small>About the Project</small></h1>
-				</div>
+				<h1><img src="/railgb/img/pins/rail-red.png" /> RailGB <small>About the Project</small> <a class="btn pull-right" href="/railgb">Back to National Rail Map</a></h1>
+			</div>
+			
 			<div class="row-fluid">
-				
-				<div class="span3">
-					<p>Insert RailGB image</p>
+				<div class="span4">
+					<img src="/railgb/img/theme/class-377-southampton.jpeg" alt="class 377 southern service train" />
 				</div>
-				<div class="span6">
-					<p>Text body</p>
+				<div class="span7 offset1">
+					<p>RailGB is a project for the annual WAIS Fest 2012. The team involved are all members of the Web & Internet Sciences (WAIS) research group at the University of Southampton. The aim of the project was to identify accessible public transport systems, primarily the UK rail services and London Underground Network.</p>
+					<p>RailGB uses linked data provided by <a href="http://oad.rkbexplorer.com/">RXKexplore</a>.</p>
+					<p>Special thanks to Hugh Glaser for providing the triple store.</p>
+					<p>The project was built by Yunjia Li, Russell Newman & Magnus White.  Data on many sites related to accessibility were collected and added to the WAIS fest wiki by Lisha Chen-Wilson, Rumi Hirabayashi, Kewalin Angkananon,  Alaa Mashat  and E.A. Draffan. Unfortunately none of these were in a format that could be used as linked data illustrating the issues arising for those who wish to access  information across the country from a single service. </p>
 				</div>
 			</div>
-		</div>	
+		</div>
+
 		<div class="container">
 			<footer>
-				<p class="pull-right muted"><a href="/railgb/about.php">About</a></p>
+				<p class="pull-right muted"><a href="/railgb/about">About</a></p>
 				<p class="pull-left"><img src="/railgb/img/theme/uos.png" alt="University of Southampton"></p>
 			</footer>
 		</div>
+		
 	</body>
 </html>
