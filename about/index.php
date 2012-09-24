faac933931b3feb1aa990a326d13e2fd5db74abf
Magnus P White <mpw@ecs.soton.ac.uk>
2012-09-24 14:21:14 +0100

    Moved site to railgb.org.uk. Tidied up code etc
    Signed-off-by: Magnus P White <mpw@ecs.soton.ac.uk>

--- about/index.php
+++ about/index.php
@@ -1,25 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<link rel="icon" href="/railgb/img/theme/favicon.png" type="image/x-icon">
-		<link href="/railgb/css/bootstrap.css" rel="stylesheet">
-		<link href="/railgb/css/bootstrap-responsive.css" rel="stylesheet">
-		<link href="/railgb/css/railgb.css" rel="stylesheet">
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
-		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<?
+			$path = $_SERVER['DOCUMENT_ROOT'];
+			include_once($path.'/includes/header.php');
+		?>
 		
 		<title>RailGB - Accessible Rail Network Map</title>
 	</head>
 
 	<body>
+	
+		<? include_once($path.'/includes/menu.php'); ?>
+	
 		<div class="container" id="container">
 			<div class="page-header">
-				<h1><img src="/railgb/img/pins/rail-red.png" /> RailGB <small>About the Project</small> <a class="btn pull-right" href="/railgb">Back to National Rail Map</a></h1>
+				<h1><img src="/public/img/pins/rail.png" alt="national rail logo" /> RailGB <small>About the Project</small></h1>
 			</div>
 			
 			<div class="row-fluid">
 				<div class="span4">
-					<img src="/railgb/img/theme/class-377-southampton.jpeg" alt="class 377 southern service train" />
+					<img src="/public/img/theme/class-377-southampton.jpeg" alt="class 377 southern service train" />
 				</div>
 				<div class="span7 offset1">
 					<p>RailGB is a project for the annual WAIS Fest 2012. The team involved are all members of the Web & Internet Sciences (WAIS) research group at the University of Southampton. The aim of the project was to identify accessible public transport systems, primarily the UK rail services and London Underground Network.</p>
@@ -30,12 +31,7 @@
 			</div>
 		</div>
 
-		<div class="container">
-			<footer>
-				<p class="pull-right muted"><a href="/railgb/about">About</a></p>
-				<p class="pull-left"><img src="/railgb/img/theme/uos.png" alt="University of Southampton"></p>
-			</footer>
-		</div>
+		<? include_once($path.'/includes/footer.php'); ?>
 		
 	</body>
 </html>
---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<div class="container">
+	<footer>
+		<p class="pull-left"><img src="/public/img/theme/uos.png" alt="University of Southampton"></p>
+	</footer>
+</div>
