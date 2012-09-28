9dd813fd626214f732fccb17b0beb287e8e83c86
Magnus P White <mpw@ecs.soton.ac.uk>
2012-09-28 11:54:43 +0100

    Added example of object with a blank node (toilets)
    Signed-off-by: Magnus P White <mpw@ecs.soton.ac.uk>

--- standard/public/ajax/tube.php
+++ standard/public/ajax/tube.php
@@ -1,26 +1,33 @@
 <?php
-$query = "PREFIX id:   <http://oad.rkbexplorer.com/id/>
+$query = "
+PREFIX id:   <http://oad.rkbexplorer.com/id/>
 PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+prefix xsd: <http://www.w3.org/2001/XMLSchema#> 
 PREFIX owl:  <http://www.w3.org/2002/07/owl#>
-PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
-PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
-PREFIX dbpprop: <http://dbpedia.org/property/>
-PREFIX dbpedia: <http://dbpedia.org/resource/>
-PREFIX fb: <http://rdf.freebase.com/ns/>
+prefix foaf:<http://xmlns.com/foaf/0.1/> 
+prefix geo:<http://www.w3.org/2003/01/geo/wgs84_pos#> 
+prefix dbpprop:<http://dbpedia.org/property/> 
+prefix tf: <http://www.railgb.org.uk/ns/2012/9/tubefacility.owl#> 
 
-SELECT Distinct ?station ?name ?long ?lat ?hasLift
+select distinct ?name ?address ?phone ?lat ?lng ?zone ?stepFree ?toilets
+where
 {
-    ?station rdf:type dbpedia-owl:Station.
-    ?station dbpprop:manager dbpedia:London_Underground.
-    ?station owl:sameAs ?freebase.
-    ?station rdfs:label ?name.
-    ?station <http://rdf.freebase.com/ns/user.sterops.accessibility.wheelchair_accessible_location.elevator> ?hasLift.
-    ?freebase fb:location.location.geolocation ?location.
-    ?location fb:location.geocode.longitude ?long.
-    ?location fb:location.geocode.latitude ?lat.
-    FILTER ( lang(?name) = 'en' )
+  ?station rdf:type tf:TubeStation.
+  ?station rdfs:label ?name.
+  ?station tf:hasAddress ?address.
+  ?station tf:hasPhone ?phone.
+  ?station geo:lat ?lat.
+  ?station geo:long ?lng.
+  ?station dbpprop:fareZone ?zone.
+  
+  ?station tf:isStepFreeStation ?stepFree.
+  Filter(?stepFree = "false"^^xsd:boolean)
+  
+  ?station tf:hasToilets ?toilets.
+  ?toilets tf:facilityAvailable ?toiletsBool.
+  Filter(?toiletsBool = "false"^^xsd:boolean)
+  
 }";
 
 $contents = file_get_contents("http://oad.rkbexplorer.com/sparql/?format=json&query=".urlencode(str_replace("\n", " ", $query)));
