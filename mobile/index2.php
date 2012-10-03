8ba7c7c59bee12ba8d166feee30e2a20e7f62a21
Yunjia Li <yl2@ecs.sotonacuk>
2012-10-03 18:41:26 +0100

    Finish filter page and list tube page Add info window on the map

--- mobile/index2.php
+++ mobile/index2.php
@@ -4,6 +4,7 @@
 		<title>Tube London - Accessible London Underground Map</title>
 		<meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />
 		<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+		<script type="text/javascript" src="http://google-maps-utility-library-v3.googlecode.com/svn/trunk/infobox/src/infobox.js"></script>
 		<?php
 			$path = $_SERVER['DOCUMENT_ROOT'];
 			require_once($path.'/includes/header.php');
@@ -14,6 +15,7 @@
 			<div data-role="header" data-theme="a" data-position="fixed">
 				<h1>Tube Map</h1>
 				<a href="#search_div" data-icon="search" data-theme="d" data-direction="reverse" data-rel="popup" data-transition="slidedown" class="ui-btn-left">Search</a>
+				<a href="#tubelist_div" data-icon="grid" data-theme="d" data-direction="reverse" class="ui-btn-right"  id="list_a">List</a>
 			</div>
 			<div data-theme="d" style="width:100%;height:100%;padding:0">
 				<div id="map-canvas" style="width:100%;height:100%;"></div>
@@ -22,19 +24,28 @@
 			<div data-role="popup" id="search_div">	
 				<div data-role="content" data-theme="d">
 					 <div style="padding:10px 20px;">
-					  <h3>Please sign in</h3>
-			          <select id="radius" name="radius">
-									<option value="0.25">0.25 mile</option>
-									<option value="0.5" selected="selected">0.5 miles</option>
-									<option value="1.0">1 miles</option>
-									<option value="2.0">2 miles</option>
-									<option value="5.0">5 miles</option>
-									<option value="10.0">10 miles</option>
-					  </select>			
+						  <h3>Search</h3>
+						  <form action="/public/ajax/tube.php" method="get" data-ajax="false" id="search_form">
+							  <input type="search" name="address" id="address" value="" data-mini=“true” placeholder="Postcode or Address"/>
+					          <select id="radius" name="radius" data-mini=“true”>
+											<option value="0.25">0.25 miles radius</option>
+											<option value="0.5" selected="selected">0.5 miles radius</option>
+											<option value="1.0">1 miles radius</option>
+											<option value="2.0">2 miles radius</option>
+											<option value="5.0">5 miles radius</option>
+											<option value="10.0">10 miles radius</option>
+							  </select>
+							  <input type="submit" value="Submit" data-theme="b"/>
+						  </form>			
 					 </div>
 			    </div>
 			</div>
 			<!-- /Search Form-->
+			<!-- Search result info form -->
+			<div data-role="popup" id="search_info_div" class="ui-content" data-theme="e" style="max-width:200px;">
+				<p id="search_info_p"></p>
+	        </div>
+			<!-- /Search result info form -->
 			<div data-role="footer" data-theme="d" data-position="fixed" >
 				<div data-role="navbar" >
 					<ul>
@@ -52,10 +63,54 @@
 			<div data-role="header" data-theme="a">
 				<h1>Filter</h1>
 				<a href="#tubemap_div" data-theme="d" data-rel="back" data-direction="reverse" class="ui-btn-left" data-icon="arrow-l">Back</a>			
+				<!-- TODO: Add a reset Button -->
+				<!-- TODO: use cookie to remember user's choices on this phone -->
 			</div>
 			<div data-role="content">
-				Filter page
+				<div data-role="fieldcontain">
+				    <fieldset data-role="controlgroup">
+				       <legend>You are:</legend>
+					   <input type="checkbox" name="wheelchair" id="wheelchair" class="custom" />
+					   <label for="wheelchair">Wheelchair user</label>
+					   
+					   <input type="checkbox" name="blind" id="blind" class="custom" />
+					   <label for="blind">Visual impair (blind)</label>
+
+				    </fieldset>
+				</div>
+				<div data-role="fieldcontain">
+				 	<fieldset data-role="controlgroup">
+				 		<legend>Accessibility:</legend>
+						<label><input type="checkbox" name="facility[]" id="filter-lifts" value="lifts" /> Lifts </label>
+						<label><input type="checkbox" name="facility[]" id="filter-th" value="th" /> Tickets Hall </label>
+						<label><input type="checkbox" name="facility[]" id="filter-es" value="es" /> Escalators </label>
+						<label><input type="checkbox" name="facility[]" id="filter-to" value="to" /> Toilets </label>
+						<label><input type="checkbox" name="facility[]" id="filter-cp" value="cp" /> Car Park </label>
+						<label><input type="checkbox" name="facility[]" id="filter-hpth" value="hpth" /> Help Points in ticket halls</label>
+						<label><input type="checkbox" name="facility[]" id="filter-hppf" value="hppf" /> Help Points on platforms</label>
+						<label><input type="checkbox" name="facility[]" id="filter-wr" value="wr" /> Waiting Room </label>
+						<label><input type="checkbox" name="facility[]" id="filter-wifi" value="wifi" /> WiFi available </label>
+						<label><input type="checkbox" name="facility[]" id="filter-sf" value="sf" /> Step-free </label>
+				 	</fieldset>
+				</div>
+				<div data-role="collapsible" data-theme="e" data-content-theme="d">
+				   <h3>Other Facilities</h3>
+				   <p>
+				   <div data-role="fieldcontain">
+					 	<fieldset data-role="controlgroup">
+							<label><input type="checkbox" name="facility[]" id="filter-gates" value="gates" /> Gates </label>
+							<label><input type="checkbox" name="facility[]" id="filter-pb" value="pb" /> Photo Booths </label>
+							<label><input type="checkbox" name="facility[]" id="filter-cm" value="cm" /> Cash Machine </label>
+							<label><input type="checkbox" name="facility[]" id="filter-pp" value="pp" /> Payphones </label>
+							<label><input type="checkbox" name="facility[]" id="filter-vm" value="vm" /> Vending Machines </label>
+							<label><input type="checkbox" name="facility[]" id="filter-br" value="br" /> Bridge </label>
+					 	</fieldset>
+					</div>
+				   </p>	
+				</div>
+							
 			</div>
+			
 			<div data-role="footer" data-theme="d" data-position="fixed" >
 				<div data-role="navbar" >
 					<ul>
@@ -68,9 +123,31 @@
 		</div>
 		<!-- /filter page -->
 		
+		<!-- tubelist page -->
+		<div data-role="page" id="tubelist_div" data-title="Tube List">
+			<div data-role="header" data-theme="a">
+				<h1>List of Tube Stations</h1>
+				<a href="#tubemap_div" data-theme="d" data-rel="back" data-direction="reverse" class="ui-btn-left" data-icon="arrow-l">Back</a>			
+			</div>
+			<div data-role="content">
+				<ul id="tubelist_ul" data-role="listview" data-filter="true" data-filter-placeholder="Filter stations...">
+				</ul>
+			</div>
+			<div data-role="footer" data-theme="d" data-position="fixed" >
+				<div data-role="navbar" >
+					<ul>
+						<li><a class="ui-btn-active ui-state-persist" href="#tubemap_div" data-role="button" data-theme="d" id="tubemap_btn">Map</a></li>
+						<li><a href="#filter_div" data-role="button" data-theme="d" id="filter_btn">Filter</a></li>
+						<li><a href="#about_div" data-role="button" data-theme="d" id="about_btn">About</a></li>
+					</ul>
+				</div>
+			</div>	
+		</div>
+		<!-- /tubelist page -->
+		
 		<!-- detail page : display the detailed information of a tube station-->
 		<div data-role="page" id="detail_div" data-theme="d" data-title="Station Detail">
-			<div data-role="header" data-theme="a">>
+			<div data-role="header" data-theme="a">
 				<h1>Station Detail</h1>
 				<a href="#tubemap_div" data-theme="d" data-rel="back" data-direction="reverse" class="ui-btn-left" data-icon="arrow-l">Back</a>
 			</div>
--- mobile/public/js/tube.js
+++ mobile/public/js/tube.js
@@ -16,6 +16,14 @@ if (typeof(Number.prototype.toRad) === "undefined") {
 	}
 }
 
+function sortStationsDisplay(prop, asc) {
+    stationsDisplayed = stationsDisplayed.sort(function(a, b) {
+        if (asc) return (a[prop] > b[prop]);
+        else return (b[prop] > a[prop]);
+    });
+}
+
+//This function clears the marker array
 function clearOverylays()
 {
 	if(markers && markers.length > 0)
@@ -27,63 +35,40 @@ function clearOverylays()
 	}
 	markers = new Array();
 }
-function checkDistance(station)
+
+//station here is the marker
+function getDistance(station)
 {
-		//console.log("currentMarker:"+currentMarker);
-		station.setVisible(true);
-		if(currentMarker != null)
-		{
-			//console.log("here");
-			var distance =-1;
-			var lat1 = currentMarker.getPosition().lat();
-			var lat2 = station.getPosition().lat();
-			var lon1 = currentMarker.getPosition().lng();
-			var lon2 = station.getPosition().lng();
-			//console.log("here2");
-			var R = 6371; // Radius of the earth in km
-			var dLat = (lat2-lat1).toRad();
-			var dLon = (lon2-lon1).toRad(); 
-			var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-				Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) * 
-				Math.sin(dLon/2) * Math.sin(dLon/2); 
-			var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-			//console.log("here3");
-			//console.log(station.getPosition().toString()+"###"+currentMarker.getPosition().toString());
-			//console.log("distance:"+distance);
-			//console.log("radius:"+radius);
-			distance = R * c *1000; // Distance in Meters
-			//console.log("distance:"+distance);
-				if(distance <= radius)
-				{
-				 	//console.log("within distance:"+station.title);
-				 	//checkAccessibility(station);
-				 	return true;
-				}
-				else
-				{
-				    station.setVisible(false);	
-				    return false;		
-				}
-		}
+	var distance =-1;
+	var lat1 = currentMarker.getPosition().lat();
+	var lat2 = station.getPosition().lat();
+	var lon1 = currentMarker.getPosition().lng();
+	var lon2 = station.getPosition().lng();
+	//console.log("here2");
+	var R = 6371; // Radius of the earth in km
+	var dLat = (lat2-lat1).toRad();
+	var dLon = (lon2-lon1).toRad(); 
+	var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+		Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) * 
+		Math.sin(dLon/2) * Math.sin(dLon/2); 
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+	//console.log("here3");
+	//console.log(station.getPosition().toString()+"###"+currentMarker.getPosition().toString());
+	//console.log("distance:"+distance);
+	//console.log("radius:"+radius);
+	distance = R * c *1000; // Distance in Meters
+	return distance;
+
 }
 			
 function fireUpStations(stations) {
 	console.log("new markers");
-	clearOverylays();
+	clearOverylays(); //clear the marker
 	stationsDisplayed = new Array();
 	var count = 0;
 	//console.log(stations.results.bindings.length);
 	$.each(stations.results.bindings, function(i, station) {
-			//console.log(station);
-			//Yunjia: use default marker image first
-			//var hasLift = {value: false, text: 'Not available'};
-			//if(station.hasLift.value === "true") {
-			//	hasLift = {value:true,text:'Lift available'};
-			//	image = '/public/img/pins/wheelchair-ok.png';
-			//} else {
-			//	image = '/public/img/pins/wheelchair-not-ok.png';
-			//}
-			
+						
 			//console.log("hasLift",station.hasLift.value);
 			
 			//Yunjia: change the image later
@@ -98,38 +83,72 @@ function fireUpStations(stations) {
 				map: map,
 				icon:image,
 				uri: station.station.value,
+				fareZone:station.zone.value,
 				title: station.name.value,
 				draggable: false,
 				visible: true
 			});
 			
-			if(checkDistance(marker))
+			if(currentMarker != null)
 			{
-				stationsDisplayed.push(marker);
+				var distance = getDistance(marker);
+				marker.distance = distance;
+				if(distance <= radius)
+				{
+				 	marker.setVisible(true);
+				 	stationsDisplayed.push(marker);
+				
+					google.maps.event.addListener(marker, 'click', function(){
+						/* Should be show detailed function
+						$.ajax({
+							dataType:"json",
+							url: "/public/ajax/detail.php",
+							data:{stationURI:marker.uri},
+							success:function(data)
+							{
+								
+							}
+						});*/
+						 
+						 var boxText = $("<div/>").addClass("infobox ui-corner-all").text(marker.title);
+						 var detail_a = $("<a/>").attr("href","#detail_div").attr("data-mini","true").attr("data-role","button").attr("data-icon","arrow-r").attr("data-iconpos","right").attr("data-theme","e").text("details").button();
+						 boxText.append(detail_a);
+						 var ibOptions = {
+							content: boxText.get(0),
+			                disableAutoPan: false,
+			                maxWidth: 280,
+			                pixelOffset: new google.maps.Size(-140, 0),
+			                zIndex: null,
+			                boxStyle:{
+				                background: "url('http://google-maps-utility-library-v3.googlecode.com/svn/trunk/infobox/examples/tipbox.gif') no-repeat",
+				                opacity: 0.75,
+				                width: "280px"
+			                },
+			                closeBoxMargin: "10px 20px 20px 20px",
+			                closeBoxURL: "http://www.google.com/intl/en_us/mapfiles/close.gif",
+			                infoBoxClearance: new google.maps.Size(1, 1),
+			                isHidden: false,
+			             	enableEventPropagation: false
+						}
+			
+						var ib= new InfoBox(ibOptions);
+						ib.open(map, marker);
+					});
+				}
+				else
+				{
+				    marker.setVisible(false);	
+				}
 			}
 			markers.push(marker);
 			count++;
-			// Marker display box
-			google.maps.event.addListener(marker, 'click', function(){
-				$.ajax({
-					dataType:"json",
-					url: "/public/ajax/detail.php",
-					data:{stationURI:marker.uri},
-					success:function(data)
-					{
-						//console.log("successful");
-						$("#station-name").html(marker.title);
-						//Yunjia: render has or doesn't have div
-						$("#station").show();
-						$("#map-canvas").hide();
-					}
-				});
-				
-			});
 		});
-		$("#alert").html(
-			'<div class="alert alert-success">Showing '+count+' stations.</div>'
-		).show();
+		
+		//sort the stations by distance
+		if(stationsDisplayed.length >0)
+		{
+			sortStationsDisplay('distance',true);
+		}
 }
 
 function initialize() {
@@ -189,9 +208,10 @@ function displayStations(address, callback)
 	if(address !== undefined && $.trim(address).length >0)
 	{
 		var selected = new Array()
-		$("#form-search :checkbox:checked").each(function(){
+		$("#filter_div :checkbox:checked").each(function(){
 			selected.push($(this).val());
 		});
+		console.log(selected);
 		var geocoder = new google.maps.Geocoder();
 		geocoder.geocode( 
 			{'address': address },
@@ -287,31 +307,72 @@ $( document ).bind( 'mobileinit', function(){
 $('#tubemap_div').live('pageshow', function(event) {
 	console.log("tubemap init");
 	
+	//TODO: Customise the navigation controls
 	google.maps.event.addDomListener(window, 'load', initialize);
 				
-	/*$("#clear_btn").click(function() {
-		if(circle != null) circle.setMap(null);
-		if(currentMarker != null) currentMarker.setMap(null);
-		
-		map.setCenter(initialLatLong);
-		map.setZoom(10);
-		
-		//Yunjia Li:
-		//$.each(markers, function(i, marker) {
-		//	marker.setVisible(true);
-		//});
-		return false;
-	});
-	
-	$(".locationlookups a").click(function() {
-		return false;
-	});
-	
-	//search nearby
-	$("#search_btn").click(function(){
-		var address = $("#address").val();
-		displayStations(address,function(){
-			//Do nothing
+});
+
+$('#tubelist_div').live('pageshow', function(event) {
+	var tubelist_ul = $("#tubelist_ul");
+	tubelist_ul.empty();
+	if(stationsDisplayed.length >0)
+	{
+		$.each(stationsDisplayed, function(i, station) {
+			var distance = station.distance;
+			var station_li = $("<li/>").appendTo(tubelist_ul);
+			var station_a = $("<a/>",{
+				href:"#detail_div",
+				html:"<h3>"+station.title+"</h3>"+
+					"<p><strong>Zone "+station.fareZone+"</strong></p>"+
+					"<p class='ui-li-aside'><strong>"+Math.floor(distance)+" m</strong></p>"
+			}).appendTo(station_li);
 		});
-	});*/
+		
+		tubelist_ul.listview('refresh');
+	}
+	else
+	{
+		tubelist_ul.html("<b>No station is found</b>");
+	}				
+});
+
+
+$("#search_form").live('submit',function(e){
+	//cache the form element for use in this function
+    var $this = $(this);
+
+    //prevent the default submission of the form
+    e.preventDefault();
+
+    //run an AJAX post request to your server-side script, $this.serialize() is the data from your form being added to the request
+    var address = $("#address").val();
+	displayStations(address,function(){	
+		//var result_div = $('#search_result_div');
+		//result_div.html("<span>"+stationsDisplayed.length+" stations found</span>");
+		$("#search_div").popup('close');
+		var resultStr;
+		if(stationsDisplayed.length >0)
+		{
+			resultStr = "<p><b>"+stationsDisplayed.length+"</b> stations found. </p>";
+			//$("#list_a").text("List("+stationsDisplayed.length+")");
+			$("#list_a").show();
+		}
+		else
+		{
+			resultStr = "<p>No station found. </p>";
+			$("#list_a").hide();
+		}
+		$("#search_info_p").html(resultStr);
+		
+		//TODO: let it position to tubelist_div, it doens't work
+		window.setTimeout(function(){
+			$("#search_info_div").popup("open",{
+				positionTo:"#tubelist_div",
+				transition:'fade'
+			});
+			window.setTimeout(function(){
+				$("#search_info_div").popup("close");
+			}, 2000)
+		}, 1000)	
+	});
 });
