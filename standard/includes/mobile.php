f22d908d4869ad65fd57fe50c8bcb5615d3cfde6
Magnus P White <mpw@ecs.soton.ac.uk>
2012-10-05 12:09:22 +0100

    Switched the main site so mobile devices go to mobile site
    Signed-off-by: Magnus P White <mpw@ecs.soton.ac.uk>

--- standard/includes/mobile.php
+++ standard/includes/mobile.php
@@ -6,8 +6,6 @@ $request = $_SERVER["REQUEST_URI"];
 // Get mobile detect class
 $detect = new Mobile_Detect();
 
-/*
 if($detect->isMobile()){
-	header("Location: http://m.railgb.org.uk$request");
+	header("Location: http://m.railgb.org.uk");
 }
-*/
